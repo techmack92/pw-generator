3f6361a9bb27ab166c06eff62c73a71a5a4a71bc
Mack <mldixon9750@gmail.com>
2023-04-06 12:57:42 -0700

    added if-statement to validate confirmation prompts; added global variables to hold arrays for password criteria selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var optionsUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var optionsLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var optionsNumeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var optionsSpecial = ['!()-.?[]_`~;:@#$%^&*+=)'];
+
 function generatePassword() {
-  // 1. Prompt the user for password criteria
-  //    a. password length (btwn 8-128)
-  //    b. upper/lower case, numbers, special characters
+  // 1. Prompt the user for password length (btwn 8-128)
   var lengthOfPw = prompt("How many characters would you like your password to contain?");
-    
-      if (lengthOfPw < 8 || lengthOfPw > 128) {
+
+  // 2. Validate length of password
+  // If user selects "cancel", end function
+  if (!lengthOfPw) {
+    return;
+  }
+  else if (lengthOfPw < 8 || lengthOfPw > 128) {
       alert("Length of password can be no less than 8 characters & no more than 128 characters.");
       return generatePassword();
     } 
 
+  // Variables that prompt user to confirm password criteria
+  var lowerCase = confirm("Click OK to confirm if you want to include lowercase characters.");
+  var upperCase = confirm("Click OK to confirm if you want to include uppercase characters.");
+  var numericChar = confirm("Click OK to confirm if you want numeric characters.");
+  var specialChar = confirm("Click OK to confirm if you want special characters.");
 
-  // var lowerCase = confirm("Click OK to confirm if you want to include lowercase characters.");
-  // var upperCase = confirm("Click OK to confirm if you want to include uppercase characters.");
-  // var numericChar = confirm("Click OK to confirm if you want numeric characters.");
-  // var specialChar = confirm("Click OK to confirm if you want special characters.");
+  // 2a. Validate confirmation of lowerCase, upperCase, numericChar & specialChar
+  if (!lowerCase && !upperCase && !numericChar && !specialChar) {
+    alert("Please enter at least one character type.");
+    return generatePassword();
+  }
 
-  // 2. Validate the input
-  //    a. validate length of password
+
+
+
+
+
+  
+
 
 
 
